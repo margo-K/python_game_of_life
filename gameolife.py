e0d7560e6ffbd7296b7d9b04c4ab256f8e0aa3c9
Margo Kulkarni <margo.r.kulkarni@gmail.com>
2013-03-25 17:02:20 -0400

    Added function for returning the coordinates of the neighbors of a cell (i,j)

--- gameolife.py
+++ gameolife.py
@@ -2,12 +2,12 @@ import numpy as np
 
 DEFAULT_PROBABILITY = np.random.random()
 
-def render(array,size,alive_check):
+def render(board_array,size,alive_check):
 	representation = ''
 
 	for j in range(size):
 		for i in range(size):
-			if alive_check(array[i,j]):
+			if alive_check(board_array[i,j]):
 				representation = representation + "X"
 			else:
 				representation = representation + "."
@@ -20,18 +20,27 @@ def check_prob(cell_value,probability=DEFAULT_PROBABILITY):
 		return True
 	return False
 
-def random_board(size):
-	board = np.random.random((size,size))
-	print DEFAULT_PROBABILITY, board
-	render(board,size,check_prob)
-
-def will_live(cell_alive,alive_neighbors):
+def check_neighbors(cell_alive,alive_neighbors):
 	if alive_neighbors == 3:
 		return True
 	else: 
 		if cell_alive and alive_neighbors ==2:
 			return True
 	return False
+
+def return_neighbors(i,j):
+	neighbors = []
+	for y_offset in [-1,0,1]:
+		for x_offset in [-1,0,1]:
+			neighbors.append((i+x_offset,j+y_offset))
+	return neighbors
+
+
+def random_board(size):
+	board = np.random.random((size,size))
+	print DEFAULT_PROBABILITY, board
+	render(board,size,check_prob)
+
 
 
 if __name__ == '__main__':
