e80ca5b502796e8a23c816148e3b450d44280618
Margo Kulkarni <margo.r.kulkarni@gmail.com>
2013-03-26 00:45:38 -0400

    Refactoring neighbors function to utilize list comprehensions instead of for loops Note: Seems to be much faster

--- gameolife.py
+++ gameolife.py
@@ -3,22 +3,23 @@ from time import sleep
 
 DEFAULT_SIZE = 100
 
-def random_board(size=DEFAULT_SIZE):
+def random_board(size):
 	return np.random.randint(2,size=(size,size))
 
 
 class Game:
 	def __init__(self,size=DEFAULT_SIZE):
-		self.size = size
-		self.board = random_board()
+		self.board = random_board(size=size)
+		self.board_dimension = self.board.shape[0]
+		print "The board dimension is {}".format(self.board_dimension)
 		self.print_board()
 
 	def update_board(self,alive_condition=None):
 		if alive_condition == None:
 			alive_condition = self.conway_condition
-		new_array = np.zeros((self.size,self.size))
-		for j in range(self.size):
-			for i in range(self.size):
+		new_array = np.zeros((self.board_dimension,self.board_dimension))
+		for j in range(self.board_dimension):
+			for i in range(self.board_dimension):
 				if alive_condition(i,j):
 					new_array[i,j]=1
 		self.board = new_array
@@ -33,14 +34,12 @@ class Game:
 				return True
 		return False 
 
-	def print_board(self,size=DEFAULT_SIZE,highlight=False): ## could map and join
+	def print_board(self,highlight=False): ## could map and join
 		string_board = ''
-		for j in range(size):
-			for i in range(size):
+		for j in range(self.board_dimension):
+			for i in range(self.board_dimension):
 				string_board+=self.represent_cell(i,j)
 			string_board+="\n"
-		# if highlight==True:
-		# 	representation = '033[94m'+ representation+'\033[0m'
 		print string_board
 
 	def represent_cell(self,i,j):
@@ -51,24 +50,23 @@ class Game:
 
 	def neighbors(self,i,j,max_value=None):
 		if max_value == None:
-			max_value = self.size
-		neighbors_list = []
+			max_value = self.board_dimension
 		count_alive = 0
-		for y_offset in [-1,0,1]:
-			for x_offset in [-1,0,1]:
-				neighbor_x,neighbor_y = i+x_offset,j+y_offset
-				if neighbor_x not in range(max_value) or neighbor_y not in range(max_value):
-					pass
-				else:
-					neighbors_list.append((neighbor_x,neighbor_y))
-		neighbors_list.remove((i,j))
-		return neighbors_list
+		offsets = [-1,0,1]
+		cartesian_product = [(x,y) for x in offsets for y in offsets if not (y==0 and x==0)]
+		neighbor_coordinates = [(x+i,y+j) for (x,y) in cartesian_product if x+i<max_value and y+j<max_value]
+		return neighbor_coordinates
 
 	def alive_neighbors(self,i,j):
 		neighbors_list = self.neighbors(i,j)
 		alive = [self.board[element[0],element[1]] for element in neighbors_list]
 		return sum(alive)
 
+# class Cell:
+# 	def __init__(self,x,y):
+# 		self.x = x
+# 		self.y = y
+# 	def 
 
 if __name__ == '__main__':
 	g = Game()
